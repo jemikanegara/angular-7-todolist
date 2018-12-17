1a2ffbe856ea62dfd7ff97937fe3bcc4ba070d93
jemikanegara <jemikanegara@gmail.com>
2018-12-17 14:38:09 +0800

    add todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { TodoComponent } from './components/todo/todo.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { TodoComponent } from "./components/todo/todo.component";
+import { HeaderComponent } from "./components/header/header.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TodoComponent,
-    HeaderComponent,
-    FooterComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, TodoComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-todo',
-  templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  selector: "app-todo",
+  templateUrl: "./todo.component.html",
+  styleUrls: ["./todo.component.css"]
 })
 export class TodoComponent implements OnInit {
+  todoList: string[];
+  addTodo: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.todoList = ["Learn Angular", "Learn Rxjs", "Learn MongoDB"];
   }
 
+  submitTodo() {
+    this.todoList.unshift(this.addTodo);
+  }
+
+  ngOnInit() {}
 }
